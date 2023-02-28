ef911fb0b522e491104418e10419b67708baf5ab
magnusmoe01 <96432497+magnusmoe01@users.noreply.github.com>
2023-02-28 09:13:59 +0100

    css

--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -10,8 +10,9 @@ const About = () => {
                     <h1>Go from this ...</h1>
                     <br />
                     <img src={images.manual} />
-                    <br /><br />
+                    <br />
                     <b>Avoid manual work</b>
+                    <br />
                     <p>
                         These are the things teachers have to stress about when creating a new map:
                     </p>
@@ -22,7 +23,8 @@ const About = () => {
                         - Who should sit in the back<br />
                         - Everyone should get a new partner<br />
                         - Everyone should get a new seat<br />
-                        <b>How can I make all of this work ?!</b>
+                        <b>- How can I make all of this work ?!</b>
+                        <br /><br />
                     </p>
                     <p>Teachers use approximately 30 minutes to make a manual map.</p>
                 </div>
@@ -39,7 +41,7 @@ const About = () => {
                     </p>
                     <b>Save money</b>
                     <p>
-                        A new map is on average generated every 3-4 weeks.
+                        A new map is on average made every 3-4 weeks.
                         A teacher in Norway has an average hourly salary of €30.
                         That would equal a cost price of €400 per teacher per year for 
                         manual work that can be automated and reduced from 30 minutes to 2 minutes.
